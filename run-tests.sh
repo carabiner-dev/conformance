9aed1edb3eec4d880165c99ffff4c202186ad3e0
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-09-20 19:12:45 -0600

    Print AMPEL version under test
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- run-tests.sh
+++ run-tests.sh
@@ -8,6 +8,10 @@ go build  -o "${TMP_DIR}/ampel" ./cmd/ampel
 AMPEL_TEST_BINARY="${TMP_DIR}/ampel"
 export AMPEL_TEST_BINARY
 
+
+"$AMPEL_TEST_BINARY" version
+echo 
+
 for f in conformance/*/verify.sh; do
     echo "🔴🟡🟢 RUNNING TEST $f"
 	 "$f"
