0df54bbf771651b1c9a544dcad6fddceda50a701
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-09-20 19:11:24 -0600

    Use prebuilt ampel binary
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- parallel/verify.sh
+++ parallel/verify.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
+source conformance/common.sh
+
 # This test executes 1000 evaluations on a binary to test ampel's parallel 
 # evaluation engine.
 
-go run ./cmd/ampel verify conformance/parallel/sourcetool \
+echo ">> 1000 Evaluations: Serially"
+time $AMPEL_TEST_BINARY verify conformance/parallel/sourcetool \
 	-a conformance/parallel/sourcetool.intoto.jsonl \
-	-p conformance/parallel/1000-evaluations.ampel.json
+	-p conformance/parallel/1000-evaluations.ampel.json \
+    --workers=1 > /dev/null
+echo ""
+
+echo ">> 1000 Evaluations: 100 Parallel Workers"
+time $AMPEL_TEST_BINARY verify conformance/parallel/sourcetool \
+	-a conformance/parallel/sourcetool.intoto.jsonl \
+	-p conformance/parallel/1000-evaluations.ampel.json \
+    --workers=100 > /dev/null
+echo ""
--- sourcetool/verify.sh
+++ sourcetool/verify.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-go run ./cmd/ampel verify conformance/sourcetool/sourcetool-v0.6.3-linux-amd64 \
+source conformance/common.sh
+
+# This test performs a simple evaluation reading a remote policy
+
+$AMPEL_TEST_BINARY verify conformance/sourcetool/sourcetool-v0.6.3-linux-amd64 \
 	-c jsonl:conformance/sourcetool/sourcetool-v0.6.3.intoto.jsonl \
 	-p 'git+https://github.com/carabiner-dev/policies#slsa/builderid-base.json' \
 	-x "builderId:https://github.com/slsa-framework/source-tool/.github/workflows/release.yaml" 
